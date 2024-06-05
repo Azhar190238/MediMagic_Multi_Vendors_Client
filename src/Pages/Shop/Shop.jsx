4113dc4e1a71ec6e05f2e148738675a1a6d1ca5e
MD AZHARUDDIN <azhar73397@gmail.com>
2024-06-06 02:50:11 +0600

    all information showing properly in details model

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,87 +1,4 @@
 
-// import UseMedicineCart from "../../Hooks/UseMedicineCart";
-// import SectionTitle from "../Shared/Section/SectionTitle";
-
-
-// const Shop = () => {
-//     const [carts] = UseMedicineCart();
-
-
-
-//     return (
-//         <div className="my-20 mx-16">
-//             <SectionTitle subHeading="Please buy here!!" heading="Shop Medicine"
-//             ></SectionTitle>
-
-//             <div className="overflow-x-auto">
-//                 <table className="table table-zebra">
-//                     {/* head */}
-//                     <thead>
-//                         <tr>
-//                             <th>SERIAL</th>
-//                             <th>IMAGE</th>
-//                             <th>Medicine NAME</th>
-//                             <th>Category</th>
-//                             <th>Company NAME</th>
-//                             <th>Item Mass Unit</th>
-//                             <th>PRICE</th>
-//                             <th>Buy</th>
-//                             <th>Details</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {carts.map((item, index) => (
-//                             <tr key={item._id}>
-//                                 <td>{index + 1}</td>
-//                                 <td>
-//                                     <div className="flex items-center gap-3">
-//                                         <div className="avatar">
-//                                             <div className="mask mask-squircle w-12 h-12">
-//                                                 <img src={item.image} alt="Medicine" />
-//                                             </div>
-//                                         </div>
-//                                     </div>
-//                                 </td>
-//                                 <td>{item.itemName}</td>
-//                                 <td>{item.category}</td>
-//                                 <td>{item.company}</td>
-//                                 <td>{item.massUnit}</td>
-//                                 <td>${item.price}</td>
-//                                 <td><button className="btn btn-primary">Add Cart</button></td>
-//                                 <td>
-//                                     <button className="btn btn-secondary" onClick={() => document.getElementById('my_modal_4').showModal()}>Details</button>
-//                                     <dialog id="my_modal_4" className="modal">
-//                                         <div className="modal-box w-11/12 max-w-5xl">
-//                                             <div className="hero min-h-screen bg-base-200">
-//                                                 <div className="hero-content flex-col lg:flex-row">
-//                                                     <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
-//                                                     <div>
-//                                                         <h1 className="text-5xl font-bold">{item.itemName}</h1>
-//                                                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-//                                                         <button className="btn btn-primary">Get Started</button>
-//                                                     </div>
-//                                                 </div>
-//                                             </div>
-//                                             <div className="modal-action">
-//                                                 <form method="dialog">
-//                                                     {/* if there is a button, it will close the modal */}
-//                                                     <button className="btn">Close</button>
-//                                                 </form>
-//                                             </div>
-//                                         </div>
-//                                     </dialog>
-//                                 </td>
-//                             </tr>
-//                         ))}
-//                     </tbody>
-//                 </table>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default Shop;
-
 
 import { useState } from 'react';
 import UseMedicineCart from "../../Hooks/UseMedicineCart";
@@ -153,18 +70,6 @@ const Shop = () => {
             {selectedItem && (
                 <dialog id="my_modal_4" className="modal">
                     <div className="modal-box w-11/12 max-w-5xl">
-                        {/* <div className="hero min-h-screen bg-base-200"> */}
-                        {/* <div className="hero-content flex-col lg:flex-row">
-                                <img src={selectedItem.image} className="max-w-sm rounded-lg shadow-2xl" alt="Medicine" />
-                                <div>
-                                    <h1 className="text-5xl font-bold">{selectedItem.itemName}</h1>
-                                    <p className="py-6">{selectedItem.description}</p>
-                                    <button className="btn btn-primary">Get Started</button>
-                                </div>
-                            </div>
-
-                        </div> */}
-
                         <div className=" bg-base-200">
                             <div className="hero-content flex-col lg:flex-row">
                                 <div className=" h-full">
